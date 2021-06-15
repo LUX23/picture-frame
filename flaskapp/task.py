452ce3cabc1f50c8c287983710ef6c7818a18738
Matvey Cherepanov <70555513+LUX23@users.noreply.github.com>
2021-06-15 13:38:46 +0700

    Update task.py

--- flaskapp/task.py
+++ flaskapp/task.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return " <html><head></head> <body> Hello World! </body></html>"
+ return " <html><head></head> <body> Hello World! </body></html>"
 
 from flask import render_template
 from flask_wtf import FlaskForm,RecaptchaField
@@ -12,7 +12,7 @@ from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 SECRET_KEY = 'secret'
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SECRET_KEY'] = SECRET_KEY 
 app.config['RECAPTCHA_USE_SSL'] = False
 app.config['RECAPTCHA_PUBLIC_KEY'] = '6Le3GjQbAAAAANa1sx3fbxugOBQlRDGjgP3nsKvu'
 app.config['RECAPTCHA_PRIVATE_KEY'] = '6Le3GjQbAAAAAJXjBb4kQb3bSPk3QRk8fqlHdHKz'
@@ -20,28 +20,28 @@ app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
 
 from flask_bootstrap import Bootstrap
 bootstrap = Bootstrap(app)
+
 class NetForm(FlaskForm):
-    size = StringField('Choose a size', validators = [DataRequired()])
-    rcolor = StringField('Choose the red level. Enter a number from 0.0 to 1', validators = [DataRequired()])
-    gcolor = StringField('Choose the green level. Enter a number from 0.0 to 1', validators = [DataRequired()])
-    bcolor = StringField('Choose the blue level. Enter a number from 0.0 to 1', validators = [DataRequired()])
-    upload = FileField('Load image', validators=[
-    FileRequired(),
-    FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
-    recaptcha = RecaptchaField()
-    submit = SubmitField('Send')
+ size = StringField('Choose a size', validators = [DataRequired()])
+ rcolor = StringField('Choose the red level. Enter a number from 0.0 to 1', validators = [DataRequired()])
+ gcolor = StringField('Choose the green level. Enter a number from 0.0 to 1', validators = [DataRequired()])
+ bcolor = StringField('Choose the blue level. Enter a number from 0.0 to 1', validators = [DataRequired()])
+ upload = FileField('Load image', validators=[
+ FileRequired(),
+ FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+ recaptcha = RecaptchaField()
+ submit = SubmitField('Send')
 
 from werkzeug.utils import secure_filename
 import os
-
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 def draw(filename,size,rcolor,gcolor,bcolor):
-    print(filename)
-    img= Image.open(filename)
+ print(filename)
+ img= Image.open(filename)
 
  fig = plt.figure(figsize=(6, 4))
  ax = fig.add_subplot()
@@ -49,8 +49,9 @@ def draw(filename,size,rcolor,gcolor,bcolor):
  ax.imshow(img, cmap='plasma')
  b = ax.pcolormesh(data, edgecolors='black', cmap='plasma')
  fig.colorbar(b, ax=ax)
- gr_path = "./static/new_graph.png"
+ gr_path = "./static/newgr.png"
  sns.displot(data)
+ #plt.show()
  plt.savefig(gr_path)
  plt.close()
 
@@ -84,7 +85,7 @@ def draw(filename,size,rcolor,gcolor,bcolor):
  img[:,0:size,2] = bcolor
  img[:,224-size:,2] = bcolor
  img[224-size:,:,2] = bcolor
- 
+
  img = Image.fromarray((img * 255).astype(np.uint8))
  print(img)
  new_path = "./static/new.png"
@@ -94,21 +95,22 @@ def draw(filename,size,rcolor,gcolor,bcolor):
 
 @app.route("/net",methods=['GET', 'POST'])
 def net():
-    form = NetForm()
-    filename=None
-    newfilename=None
-    grname=None
-    if form.validate_on_submit():
-        filename = os.path.join('./static', secure_filename(form.upload.data.filename))
-        sz=form.size.data
-        sr=form.rcolor.data
-        sg=form.gcolor.data
-        sb=form.bcolor.data
+ form = NetForm()
+ filename=None
+ newfilename=None
+ grname=None
+ if form.validate_on_submit():
+  filename = os.path.join('./static', secure_filename(form.upload.data.filename))
  
-        form.upload.data.save(filename)
-        newfilename, grname = draw(filename,sz,sr,sg,sb)
+  sz=form.size.data
+  sr=form.rcolor.data
+  sg=form.gcolor.data
+  sb=form.bcolor.data
+  
+  form.upload.data.save(filename)
+  newfilename, grname = draw(filename,sz,sr,sg,sb)
  
-    return render_template('net.html',form=form,image_name=newfilename,gr_name=grname)
+ return render_template('net.html',form=form,image_name=newfilename,gr_name=grname)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port=5000)
+ app.run(host='127.0.0.1',port=5000)
