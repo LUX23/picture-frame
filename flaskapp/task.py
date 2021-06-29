1a725f492b3cb98d21012d0b7bcade56d32fe72f
Matvey Cherepanov <70555513+LUX23@users.noreply.github.com>
2021-06-29 16:37:47 +0700

    Update task.py

--- flaskapp/task.py
+++ flaskapp/task.py
@@ -4,25 +4,31 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
  return " <html><head></head> <body> Hello World! </body></html>"
-
 from flask import render_template
 from flask_wtf import FlaskForm,RecaptchaField
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-
 SECRET_KEY = 'secret'
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['RECAPTCHA_USE_SSL'] = False
 app.config['RECAPTCHA_PUBLIC_KEY'] = '6Le3GjQbAAAAANa1sx3fbxugOBQlRDGjgP3nsKvu'
 app.config['RECAPTCHA_PRIVATE_KEY'] = '6Le3GjQbAAAAAJXjBb4kQb3bSPk3QRk8fqlHdHKz'
 app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
-
 from flask_bootstrap import Bootstrap
 bootstrap = Bootstrap(app)
 
-class ImageForm(FlaskForm):
+class NetForm(FlaskForm):
  size = StringField('Size', validators = [DataRequired()])
+
+ r_out = StringField('Выберите уровень красного для внешней рамки. От 0.0 до 1.', validators = [DataRequired()])
+ g_out = StringField('Выберите уровень зеленого для внешней рамки. От 0.0 до 1.', validators = [DataRequired()])
+ b_out = StringField('Выберите уровень синего для внешней рамки. От 0.0 до 1.', validators = [DataRequired()])
+
+ r_in = StringField('Выберите уровень красного для внутренней рамки. От 0.0 до 1.', validators = [DataRequired()])
+ g_in = StringField('Выберите уровень зеленого для внутренней рамки. От 0.0 до 1.', validators = [DataRequired()])
+ b_in = StringField('Выберите уровень синего для внутренней рамки. От 0.0 до 1.', validators = [DataRequired()])
+
  upload = FileField('Load image', validators=[
  FileRequired(),
  FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
@@ -34,8 +40,7 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-def frame(filename,size):
+def draw(filename,size):
  print(filename)
  img= Image.open(filename)
  fig = plt.figure(figsize=(6, 4))
@@ -44,7 +49,7 @@ def frame(filename,size):
  ax.imshow(img, cmap='plasma')
  b = ax.pcolormesh(data, edgecolors='black', cmap='plasma')
  fig.colorbar(b, ax=ax)
- graph = "./static/graph.png"
+ gr_path = "./static/graph.png"
  sns.displot(data)
  #plt.show()
  plt.savefig(gr_path)
@@ -60,25 +65,21 @@ def frame(filename,size):
  img[224-size:,:,1] = 1
  img = Image.fromarray((img * 255).astype(np.uint8))
  print(img)
- new = "./static/new.png"
+ new_path = "./static/new.png"
  print(img)
  img.save(new_path)
- return new, graph
-
+ return new_path, gr_path
 @app.route("/net",methods=['GET', 'POST'])
 def net():
- form = ImageForm()
+ form = NetForm()
  filename=None
  newfilename=None
  grname=None
  if form.validate_on_submit():
-  filename = os.path.join('./static',
-secure_filename(form.upload.data.filename))
+  filename = os.path.join('./static', secure_filename(form.upload.data.filename))
   sz=form.size.data
   form.upload.data.save(filename)
-  newfilename, grname = frame(filename,sz)
- return
-render_template('net.html',form=form,image_name=newfilename,gr_name=grname)
-
+  newfilename, grname = draw(filename,sz)
+ return render_template('net.html',form=form,image_name=newfilename,gr_name=grname)
 if __name__ == "__main__":
  app.run(host='127.0.0.1',port=5000)
